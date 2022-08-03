972ca397adcb0a22fd9feb285a5cff95fb7e8c59
Marija Nikolic <m.nikolic1144@gmail.com>
2022-08-03 18:31:19 +0200

    dog pics

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,27 @@
-export default function DogPics () {
+import { useEffect, useState } from "react";
+
+export default function DogPics() {
+  const [image, setImage] = useState();
   // API: https://dog.ceo/dog-api/
+
+  const getNewImage = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((response) => setImage(response.message));
+  };
+
+  useEffect(() => {
+    getNewImage();
+  }, []);
+
   return (
-    <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+    <div className="dog-pics">
+      {image ? (
+        <img src={image} alt="random dog pic" />
+      ) : (
+        <p>Loading image...</p>
+      )}
+      <button onClick={() => getNewImage()}>🐶</button>
     </div>
-  )
+  );
 }
