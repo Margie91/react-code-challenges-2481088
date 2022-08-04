4544ebace781707a24ce278bc6592f3de22ba13b
Marija Nikolic <m.nikolic1144@gmail.com>
2022-08-04 12:42:07 +0200

    score

--- src/05-score-keeper/ScoreKeeper.js
+++ src/05-score-keeper/ScoreKeeper.js
@@ -1,13 +1,20 @@
-import { useEffect, useState } from 'react'
+import { useState } from "react";
+import { useEffect } from "react/cjs/react.development";
 
-export default function ScoreKeeper () {
-  const [score, setScore] = useState(0)
+export default function ScoreKeeper() {
+  const [score, setScore] = useState(
+    parseInt(localStorage.getItem("score")) || 0
+  );
+
+  useEffect(() => {
+    localStorage.setItem("score", score);
+  }, [score]);
 
   return (
     <div>
       <h1>Your score is: {score}</h1>
-      <button onClick={() => setScore(prevScore => prevScore + 1)}>+</button>
-      <button onClick={() => setScore(prevScore => prevScore - 1)}>-</button>
+      <button onClick={() => setScore((prevScore) => prevScore + 1)}>+</button>
+      <button onClick={() => setScore((prevScore) => prevScore - 1)}>-</button>
     </div>
-  )
+  );
 }
