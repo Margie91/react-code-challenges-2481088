e9622d7d890560bcb2c5c0b855bc63a4b592c6da
Marija Nikolic <m.nikolic1144@gmail.com>
2022-08-05 18:11:06 +0200

    window event

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,5 +1,14 @@
-export default function WindowEvent () {
-  return (
-    <h2>Window event active</h2>
-  )
+import { useEffect } from "react";
+
+export default function WindowEvent() {
+  const doubleClick = () => alert("double click");
+
+  useEffect(() => {
+    window.addEventListener("dblclick", doubleClick);
+
+    return () => {
+      window.removeEventListener("dblclick", doubleClick);
+    };
+  }, []);
+  return <h2>Window event active</h2>;
 }
