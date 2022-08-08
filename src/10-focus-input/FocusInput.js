4491e7de171067c4c7028e7d28b25930b36a498f
Marija Nikolic <m.nikolic1144@gmail.com>
2022-08-08 13:31:49 +0200

    ref

--- src/10-focus-input/FocusInput.js
+++ src/10-focus-input/FocusInput.js
@@ -1,8 +1,14 @@
+import { useEffect, useRef } from "react";
+
 export default function FocusInput() {
+  const ref = useRef();
+  useEffect(() => {
+    ref.current.focus();
+  }, []);
   return (
     <div>
-      <label htmlFor='focused-input'>Focus me on page load!</label>
-      <input name='focused-input'></input>
+      <label htmlFor="focused-input">Focus me on page load!</label>
+      <input name="focused-input" ref={ref}></input>
     </div>
-  )
+  );
 }
